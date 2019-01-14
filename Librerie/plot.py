1635ff2b7a7ab2dee155add73aced3c23ca33fb3
Fabio <45599613+docb115@users.noreply.github.com>
2019-01-14 15:38:45 +0100

    Update plot.py

--- Librerie/plot.py
+++ Librerie/plot.py
@@ -1,21 +1,20 @@
 import matplotlib.pyplot as plt
 from pandas import read_csv
 
-def plot(s):
-    names = ["PyDictionary","CustomDictionary_Parametri_Buoni","CustomDictionary_Parametri_Cattivi"] #filenames
+def plot():
+    names = ["DHEAP3","DHEAP10","DHEAP100"]#,"BINARH","BINOMH"] #filenames
+    option = ["_FullEdges=False","_FullEdges=True"]
     c = 0       #color
-    for f in names:
-        d = read_csv("../Data/" + s + f + ".csv", sep = ',', header = None)  # read file
-        x = d[0].values
-        y = d[1].values
-        plt.plot(x, y, color = "C" + str(c), label = f)
-        plt.legend(loc = "best")
-        c += 1
-    plt.savefig("../Graph/"+s+".png")
-    plt.show()
+    for o in option:
+        for f in names:
+            d = read_csv("../Data/CustomGraph_QueueType=" + f + o + ".csv", sep = ',', header = None)  # read file
+            x = d[0].values
+            y = d[1].values
+            plt.plot(x, y, color = "C" + str(c), label = f)
+            plt.legend(loc = "best")
+            c += 1
+        plt.savefig("../Graph/DHEAP" + o +"+.png")
+        plt.show()
 
 if __name__ == "__main__":
-    plot("Utilizzo")
-    plot("Insert")
-    plot("Search")
-    plot("Deleate")
+    plot()
